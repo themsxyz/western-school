0f03311430fa984d20c3085a8d6f3a33898c2c35
muktadir <themsxyz@gmail.com>
2026-04-27 06:16:32 +0000

    update files

---
+++ student-sms.js
@@ -0,0 +1,46 @@
+
+const CsvScriptURL = "https://script.google.com/macros/s/AKfycbwnjjd6J9OjKuZ9000cMTPgszbxSq9GDjcrCWTheLwNIeCY8cpMV-0Oc2jnhCPRA5Y9nA/exec";
+
+function CsvLog(msg) {
+  document.getElementById("CsvOutput").textContent = msg;
+}
+
+function CsvUpload() {
+  var file = document.getElementById("CsvFile").files[0];
+  if (!file) return CsvLog("Select a file first");
+
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    fetch(CsvScriptURL, {
+      method: "POST",
+      body: e.target.result
+    })
+    .then(res => res.text())
+    .then(data => CsvLog("Upload Done: " + data))
+    .catch(err => CsvLog("Error: " + err));
+  };
+
+  reader.readAsText(file);
+}
+
+function CsvSync() {
+  fetch(CsvScriptURL)
+    .then(res => res.json())
+    .then(data => CsvLog("Sync: " + JSON.stringify(data, null, 2)))
+    .catch(err => CsvLog("Error: " + err));
+}
+
+function CsvRefresh() {
+  CsvLog("Refreshing...");
+  CsvSync();
+}
+
+function CsvClear() {
+  let url = CsvScriptURL + "?mode=admin&reset=true";
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => CsvLog("Cleared: " + JSON.stringify(data)))
+    .catch(err => CsvLog("Error: " + err));
+}
+
