dbb544095c58e4effe2c2fd48ef82ff2d917f04b
muktadir <themsxyz@gmail.com>
2026-04-28 20:40:17 +0600

    Update student-account.js

--- student-account.js
+++ student-account.js
@@ -76,7 +76,7 @@
     // ---------- API ENDPOINTS ----------
     const CLASS_API_MAP = {
         nursery: "https://script.google.com/macros/s/AKfycbzRBVqJZnQCez3AS27DIMNqc83NnkDBdzUs4IZfmIsn2qOxkOe1_DM8NQvMjCPtwwiS/exec",
-        play: "https://script.google.com/macros/s/AKfycbzhtst-Y7Z4BNtDNW76zginGzhVJ9CCYM8WOot2Ij1IzPLrtxVIb6p7JuDT_ZOhgiKi/exec",
+        play: "https://script.google.com/macros/s/AKfycbywJ-GSQgRnMFg2NUQHrHa8Qs6-fKi46s4Ej8-nTacyqIpuhplr1AgPaX7Dd9gyHjqwIw/exec",
         kg: "https://script.google.com/macros/s/AKfycbxRDeg7egxUdLpjdQg8d37WvcNw1xQMd-QpfwnqC3Si2hWh7HCYjE8jBvzAqWb4ED0/exec",
         class1: "https://script.google.com/macros/s/AKfycby9Fv1xZGyZwNAfDFOKVC6Cf7q86GMz4cWvxO4u-jeC8ejMAaLc8rgmx2KDESAA134T/exec",
         class2: "https://script.google.com/macros/s/AKfycbyxfRJFIkoi5IZabxs1MiVqBNb5HgIWUR2nG0TjXLf1S7AXyW8uGMFVlJ009pXLY4JnfA/exec",
